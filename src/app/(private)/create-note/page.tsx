75d5ddd04e54c4f25f14962b406db2dacd0fcf2b
adhitamafikri <adhitamafikri.developer@gmail.com>
2025-05-14 02:09:55 +0700

    feat(shadcn): added several components

--- src/app/(private)/create-note/page.tsx
+++ src/app/(private)/create-note/page.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function CreateNote(): React.ReactNode {
-  return <h1>This is the create note page</h1>;
+  return (
+    <div>
+      <h1>This is the create note page</h1>
+      <form name="create-note-form">
+        <Input placeholder="Your Note's Title" />
+        <Textarea placeholder="Type your Notes here" />
+      </form>
+    </div>
+  );
 }
